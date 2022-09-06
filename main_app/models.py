ad0ea76076a72a54f679c9f9b7811c2c0b7eced3
kmcclendon <kmcclendon56@gmail.com>
2022-09-06 13:45:08 -0500

    Adding some comments

--- main_app/models.py
+++ main_app/models.py
@@ -17,9 +17,11 @@ class Quiz(models.Model):
     title = models.CharField(max_length=100)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
 
+
+
 # class Questions(models.Model):
 class Question(models.Model):
-    question = models.CharField(max_length=250)
+    question = models.TextField(max_length=250)
     answer = models.TextField(max_length=250)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
 
